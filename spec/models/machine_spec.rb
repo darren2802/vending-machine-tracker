7a8fa74c4eafecd37b446e90eca66c53b294d28f
Darren Campbell <darren2802@gmail.com>
2019-12-19 10:07:02 -0700

    Add method test to machine model spec

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Machine, type: :model do
     it { should have_many :machine_snacks}
     it { should have_many(:snacks).through(:machine_snacks)}
   end
+
+  describe "methods" do
+    it 'can display the average price of all snacks at a particular machine' do
+      owner = Owner.create(name: "Sam's Snacks")
+
+      dons  = owner.machines.create(location: "Don's Mixed Drinks")
+      wills = owner.machines.create(location: "Will's Snack Stop")
+
+      burger = dons.snacks.create(name: 'White Castle Burger', price: 3.50)
+      pop_rocks = dons.snacks.create(name: 'Pop Rocks', price: 1.50)
+      cheetos = wills.snacks.create(name: 'Flaming Hot Cheetos', price: 2.50)
+
+      expect(dons.avg_snack_price).to eq(2.5)
+    end
+  end
 end
